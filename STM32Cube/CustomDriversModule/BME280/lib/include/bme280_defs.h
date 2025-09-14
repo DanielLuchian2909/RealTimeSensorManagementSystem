c0f52e5cc659a715fcab111469a6d503e262337e
Daniel Luchian <95716827+DanielLuchian2909@users.noreply.github.com>
2025-09-14 18:36:16 -0400

    Revert "Added concurrent environmental sensor read write demo"

--- STM32Cube/CustomDriversModule/BME280/lib/include/bme280_defs.h
+++ STM32Cube/CustomDriversModule/BME280/lib/include/bme280_defs.h
@@ -62,7 +62,6 @@
 /******************************************************************************/
 /*! @name        Compiler switch macros Definitions                */
 /******************************************************************************/
-#define BME280_32BIT_ENABLE
 #ifndef BME280_64BIT_ENABLE /*< Check if 64-bit integer (using BME280_64BIT_ENABLE) is enabled */
 #ifndef BME280_32BIT_ENABLE /*< Check if 32-bit integer (using BME280_32BIT_ENABLE) is enabled */
 #ifndef BME280_DOUBLE_ENABLE /*< If any of the integer data types not enabled then enable BME280_DOUBLE_ENABLE */
